85e0946ff5dfc4fb76df0f72ef5d7632ec178707
Okafor Chidiebere Nelson <nelsonchidiebere14@gmail.com>
2023-01-20 15:25:07 -0800

    Nelson

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ const headerNavItemSpan = document.querySelector(".header-nav-item-span");
 
 navToggleCont.addEventListener("click", () => {
   mainNavContainer.classList.toggle("display");
-  console.log("An icon or the box icon was just clicked");
+  console.log("An icon or the icon's box was just clicked");
 });
